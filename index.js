7b572a7c2a32ec248118843bdfe2c5be9f714ad7
Isha <isha@gmail.com>
2024-12-26 10:46:12 +0530

    added body parser

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
 const urlRoute = require("./routes/url");
+
 const { connectMongoDb } = require("./connection");
 
 const app = express();
 const PORT = process.env.PORT || 8001;
 
+app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
@@ -17,11 +19,7 @@ connectMongoDb("mongodb://127.0.0.1:27017/short-url").then(() => {
   console.log("mongodb connected");
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.use("/", urlRoute);
-
 app.listen(PORT, () => {
   console.log(`server is running at port no ${PORT}`);
 });
