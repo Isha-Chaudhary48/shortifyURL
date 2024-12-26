5fe970299046c3509fb15a611062215eddcc8423
Isha <isha@gmail.com>
2024-12-26 10:48:19 +0530

    added responses that i want to show on client side

--- routes/url.js
+++ routes/url.js
@@ -1,11 +1,11 @@
 const express = require("express");
+const URL = require("../models/url");
+const router = express.Router();
 const {
   handleGenerateNewShortUrl,
   handleGetShortUrl,
   handleGetAnalytics,
 } = require("../controllers/url");
-
-const router = express.Router();
 
 router.get("/index", (req, res) => {
   console.log("rendering index page");
@@ -14,6 +14,10 @@ router.get("/index", (req, res) => {
     body: "index",
     stylesheet: "index.css",
     title: "home page",
+    shortId: "",
+    totalClicks: "",
+    visitHistory: "",
+    redirectURL: "",
   });
 });
 
@@ -23,6 +27,10 @@ router.get("/analytics", (req, res) => {
     body: "analytics",
     title: "analytics",
     stylesheet: "analytics.css",
+    shortId: "",
+    totalClicks: "",
+    visitHistory: "",
+    redirectURL: "",
   });
 });
 
@@ -32,6 +40,10 @@ router.get("/signUp", (req, res) => {
     body: "signUp",
     title: "sign up page",
     stylesheet: "signUp.css",
+    shortId: "",
+    totalClicks: "",
+    visitHistory: "",
+    redirectURL: "",
   });
 });
 
@@ -41,8 +53,75 @@ router.get("/signIn", (req, res) => {
     body: "signIn",
     title: "sign in page",
     stylesheet: "signUp.css",
+    shortId: "",
+    visitHistory: "",
+    totalClicks: "",
+    redirectURL: "",
   });
 });
+
+router.post("/generate", async (req, res) => {
+  try {
+    const { url } = req.body;
+
+    console.log(url);
+    if (!url) {
+      return res.status(400).send("url is required");
+    }
+
+    const exists = await URL.findOne({ url });
+    if (exists) {
+      return res.render("layout", {
+        body: "index",
+        stylesheet: "index.css",
+        title: "home page",
+        shortId: exists.shortId,
+        totalClicks: exists.visitHistory.length,
+        visitHistory: exists.visitHistory,
+        redirectURL: exists.redirectURL,
+      });
+    }
+
+    const newShortUrl = await handleGenerateNewShortUrl({ url });
+    return res.render("layout", {
+      body: "index",
+      stylesheet: "index.css",
+      title: "home page",
+      shortId: newShortUrl.shortId,
+      totalClicks: newShortUrl.visitHistory.length,
+      visitHistory: newShortUrl.visitHistory,
+      redirectURL: newShortUrl.redirectURL,
+    });
+  } catch (error) {
+    console.log("error occured", error);
+  }
+});
+
+router.post("/data", async (req, res) => {
+  try {
+    const { shortId } = req.body;
+    if (!shortId) {
+      return res.json({ msg: "enter shortId" });
+    }
+    const exists = await URL.findOne({ shortId });
+    if (exists) {
+      return res.render("layout", {
+        body: "analytics",
+        stylesheet: "analytics.css",
+        title: "analytics",
+        shortId: exists.shortId,
+        totalClicks: exists.visitHistory.length,
+        visitHistory: exists.visitHistory,
+        redirectURL: exists.redirectURL,
+      });
+    } else {
+      return res.send("shortId not found");
+    }
+  } catch (error) {
+    console.log("Error found in fetching analytics");
+  }
+});
+
 router.post("/", handleGenerateNewShortUrl);
 router.get("/:shortId", handleGetShortUrl);
 router.get("/analytics/:shortId", handleGetAnalytics);
