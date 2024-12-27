71ba7748c3afffa47548237e62a69b3487a88289
Isha <isha@gmail.com>
2024-12-27 15:57:10 +0530

    added routes path to the main file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const path = require("path");
 const urlRoute = require("./routes/url");
+const userRoute = require("./routes/user");
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
 
 const { connectMongoDb } = require("./connection");
 
@@ -9,17 +12,22 @@ const PORT = process.env.PORT || 8001;
 
 app.use(express.json()); // Parse JSON bodies
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-connectMongoDb("mongodb://127.0.0.1:27017/short-url").then(() => {
+connectMongoDb(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
   console.log("mongodb connected");
 });
 
 app.use("/", urlRoute);
+app.use("/user", userRoute);
 app.listen(PORT, () => {
   console.log(`server is running at port no ${PORT}`);
 });
